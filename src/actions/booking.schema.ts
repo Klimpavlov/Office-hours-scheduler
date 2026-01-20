aa2e63395d11a88d23c5a7b46ed22ab68599073f
Klimpavlov <Klimpavlov2002@gmail.com>
2026-01-20 13:27:42 +0300

    add: booking action

---
+++ src/actions/booking.schema.ts
@@ -0,0 +1,8 @@
+import z from "zod"
+export const createBookingSchema = z.object({
+    specialistId: z.string().min(1),
+    startsAt: z.string().datetime(),
+    endsAt: z.string().datetime(),
+})
+
+export type CreateBookingInput = z.infer<typeof createBookingSchema>;
