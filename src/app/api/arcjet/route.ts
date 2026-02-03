56d6f9865bb0f456a8e902ed1269707c8a224aaa
Klimpavlov <Klimpavlov2002@gmail.com>
2026-02-03 13:15:48 +0300

    fix: arcjet rate limit

--- src/app/api/arcjet/route.ts
+++ src/app/api/arcjet/route.ts
@@ -1,10 +1,17 @@
-import arcjet, { detectBot } from "@arcjet/next";
-import { isSpoofedBot } from "@arcjet/inspect";
+import arcjet, { detectBot, tokenBucket } from "@arcjet/next";
 import { NextResponse } from "next/server";
 
 const aj = arcjet({
   key: process.env.ARCJET_KEY!, // Get your site key from https://app.arcjet.com
   rules: [
+    // Create a token bucket rate limit. Other algorithms are supported.
+    tokenBucket({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      characteristics: ["userId"], // track requests by a custom user ID
+      refillRate: 5, // refill 5 tokens per interval
+      interval: 10, // refill every 10 seconds
+      capacity: 10, // bucket maximum capacity of 10 tokens
+    }),
     detectBot({
       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
       // Block all bots except the following
@@ -20,31 +27,22 @@ const aj = arcjet({
 });
 
 export async function GET(req: Request) {
-  const decision = await aj.protect(req);
+  const userId = "user123"; // Replace with your authenticated user ID
+  const decision = await aj.protect(req, { userId, requested: 5 }); // Deduct 5 tokens from the bucket
+  console.log("Arcjet decision", decision);
 
   if (decision.isDenied()) {
-    if (decision.reason.isBot()) {
+    if (decision.reason.isRateLimit()) {
       return NextResponse.json(
-        { error: "No bots allowed", reason: decision.reason },
-        { status: 403 },
+        { error: "Too Many Requests", reason: decision.reason },
+        { status: 429 },
       );
-    } else {
+    } else if (decision.reason.isBot()) {
       return NextResponse.json(
-        { error: "Forbidden", reason: decision.reason },
+        { error: "Bot detected", reason: decision.reason },
         { status: 403 },
       );
     }
-  }
-
-  // Paid Arcjet accounts include additional verification checks using IP data.
-  // Verification isn't always possible, so we recommend checking the decision
-  // separately.
-  // https://docs.arcjet.com/bot-protection/reference#bot-verification
-  if (decision.results.some(isSpoofedBot)) {
-    return NextResponse.json(
-      { error: "Forbidden", reason: decision.reason },
-      { status: 403 },
-    );
   }
 
   return NextResponse.json({ message: "Hello world" });
